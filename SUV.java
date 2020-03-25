e3feae5645b1ad226e2db9e9023594f7ce05194e
raypan891108 <raypan891108@gmail.com>
2020-03-25 19:17:16 +0800

    SUV

--- SUV.java
+++ SUV.java
@@ -1,6 +1,7 @@
 class SUV extends Car{
     private String maxpower,peoplenum;
     public SUV(){
+        super();
         String newmaxpower,newpeoplenum;
         System.out.print("maxpower:");
         newmaxpower = scn.nextLine();
@@ -13,12 +14,7 @@ class SUV extends Car{
 
     public void showinfo(){
         System.out.println("SUV");
-        System.out.println("color:" + getcolor());
-        System.out.println("capacity:" + getcapacity());
-        System.out.println("powertype:" + getpowertype());
-        System.out.println("weight:" + getweight());
-        System.out.println("displacement:" + getdisplacement());
-        System.out.println("year:" + getyear());
+        super.showinfo();
         System.out.println("maxpower:" + getmaxpower());
         System.out.println("peoplenum:" + getpeoplenum() + "\n");
     }
